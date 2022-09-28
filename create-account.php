bb7c44e65f181fe84db5970a43faf2b212ace2a1
hashmaniac <mbithidenis3@gmail.com>
2022-09-28 22:13:17 +0300

    fetch

--- create-account.php
+++ create-account.php
@@ -8,15 +8,21 @@ $libDB = new DB();
 
 //Create new Account
 $pass = $libDB->exec(
-    "INSERT INTO 'users' ('username', 'email', 'password') VALUES (?, ?, ?)", ["johndoe", "johndoe@example.com", "12345678"]
+    "INSERT INTO `users` (`username`, `email`, `password`) VALUES (?, ?, ?)", ["johndoe", "johndoe@example.com", "12345678"]
 );
 echo $pass ? "OK" : $libDB->error;
 
 //If 'ref id' exists, insert into referral table the ref id and username of acoount being registered
-if(isset($_GET('ref'))) {
-    $referer = $_GET('ref');
-    $from = $libDB->exec(
-        "INSERT INTO 'referral' ('username', 'referer') VALUES (?, ?)", ["johndoe", $referer]
+if($_GET('ref')) {
+    $referral = $libDB->fetch(
+        "SELECT * from `users` WHERE `username`=?", [$_GET['ref']]
     );
+
+    if(is_array($referral)) {
+        $referer = $_GET('ref');
+        $from = $libDB->exec(
+            "INSERT INTO `referral` (`username`, `referer`) VALUES (?, ?)", ["johndoe", $referer]
+    );
+    }
 }
 
