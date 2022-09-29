fe021ad8c77ad142e67ba21f9e5a0705f40175a6
hashmaniac <mbithidenis3@gmail.com>
2022-09-29 17:38:27 +0300

    link

---
+++ referral-link.php
@@ -0,0 +1,17 @@
+<?php
+// Generating Referral links
+
+//INIT
+require "app/config.php";
+require PATH_LIB . "lib.php";
+$libDB = new DB();
+session_start();
+
+$url = "http://localhost/referalsys/create-account.php";
+
+//first attach a session when login is a success on your login logic
+// e.g $_SESSION['userid'] = $username
+
+if(isset($_SESSION('userid'))) {
+    echo $url . "?refer=" . $_SESSION('userid');
+}
