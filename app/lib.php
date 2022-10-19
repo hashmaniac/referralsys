d940bf7f619fad32257eb7586a3a0dac5e44723c
hashmaniac <mbithidenis3@gmail.com>
2022-10-19 13:22:51 +0300

    commit

--- app/lib.php
+++ app/lib.php
@@ -9,11 +9,11 @@ class DB {
         //param: DB_HOST< DB_USER, DB_PASSWORD, DB_NAME 
 
         //Create connection
-        $conn = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+        $this->conn = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 
         //check connection
-        if ($conn->connect_error) {
-            die("Connection failed: " . $conn->connect_error);
+        if ($this->conn->connect_error) {
+            die("Connection failed: " . $this->conn->connect_error);
         }
     }
 
